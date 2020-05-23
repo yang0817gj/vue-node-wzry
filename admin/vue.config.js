98002afc134259c26df3c27d5b847f143441b12e
yang0817gj <yang0817gj@163.com>
2020-05-23 18:05:22 +0800

    vue-cli反向代理

---
+++ admin/vue.config.js
@@ -0,0 +1,25 @@
+// // vue.config.js
+// const config = {
+//     // ...
+//     devServer: {
+//         hot: true,
+//         open: true,
+//         host: '127.0.0.1',
+//         // host: '0.0.0.0',//如果是真机测试，就使用这个IP
+//         port: 8899,
+//         https: false,
+//         hotOnly: false,
+//         // proxy: {  // 这是vue-cli3
+//         //     '/api': {
+//         //         target: 'http://xxx.xxx.xx.xx:xxxx/'
+//         //     }
+//         // }
+
+//     }
+// }
+// // proxyTable: {  // 这是vue-cli2
+// //     '/api': {
+// //        target: 'http://localhost:8080',
+// //     }
+// //   },
+// module.exports = config;
